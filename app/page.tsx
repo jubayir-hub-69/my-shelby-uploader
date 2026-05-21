07dd6a93342bc93d0e50557716ccfcfc04e9f006
jubayir-hub-69 <jubayir82@gmail.com>
2026-05-21 13:15:42 +0600

    petra wallet fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,14 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [account, setAccount] = useState("");
-  const [fileName, setFileName] = useState("");
+  const [wallet, setWallet] = useState("");
 
   const connectWallet = async () => {
     try {
-      if ("aptos" in window) {
-        const wallet = (window as any).aptos;
-
-        const response = await wallet.connect();
-
-        setAccount(response.address);
-
-        alert("Wallet Connected ✅");
-      } else {
-        alert("Petra Wallet install করুন");
-      }
+      const response = await window.petra?.connect();
+      setWallet(response.address);
     } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleFile = (e: any) => {
-    const file = e.target.files[0];
-
-    if (file) {
-      setFileName(file.name);
+      alert("Petra wallet not found");
     }
   };
 
@@ -36,57 +18,23 @@ export default function Home() {
     <main className="min-h-screen bg-black text-white">
 
       <header className="flex justify-end p-6">
-
         <button
           onClick={connectWallet}
-          className="bg-blue-600 hover:bg-blue-700 px-5 py-2 rounded-lg"
+          className="bg-blue-600 px-5 py-2 rounded-lg"
         >
-          {account
-            ? account.slice(0,6) +
-              "..." +
-              account.slice(-4)
-            : "Connect Wallet"}
+          {wallet ? wallet.slice(0,6)+"..."+wallet.slice(-4) : "Connect Wallet"}
         </button>
-
       </header>
 
       <div className="flex justify-center items-center h-[80vh]">
 
-        <div className="bg-zinc-900 p-8 rounded-2xl w-[420px] text-center shadow-lg">
+        <div className="bg-zinc-900 p-8 rounded-xl w-[420px]">
 
-          <h1 className="text-4xl font-bold mb-3">
+          <h1 className="text-3xl font-bold text-center mb-5">
             Shelby Storage
           </h1>
 
-          <p className="text-gray-400 mb-8">
-            Secure decentralized storage
-          </p>
-
-          <label
-            htmlFor="file"
-            className="border-2 border-dashed border-gray-600 rounded-xl p-10 block cursor-pointer hover:border-blue-500"
-          >
-
-            📁 Upload File
-
-          </label>
-
-          <input
-            id="file"
-            type="file"
-            hidden
-            onChange={handleFile}
-          />
-
-          {fileName && (
-
-            <p className="mt-4 text-green-400">
-
-              {fileName}
-
-            </p>
-
-          )}
+          <input type="file"/>
 
         </div>
 
